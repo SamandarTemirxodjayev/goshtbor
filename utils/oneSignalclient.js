40719dd45a35cb12c91659e04bfd966e5e5d35a6
Samandar <iwtuz166@gmail.com>
2024-04-08 13:59:08 +0500

    change onesignalid

--- utils/oneSignalclient.js
+++ utils/oneSignalclient.js
@@ -1,8 +1,8 @@
 const OneSignal = require("onesignal-node");
 
 const client = new OneSignal.Client(
-	"7bb12952-573d-4341-afbf-3b451928b008",
-	"NDY0NjIxMTctNmJjMy00NDg1LTg1ZTctOGNhZWE3NjcwYjdk",
+	"fa42a951-2647-4c7a-b1a4-1403203415a6",
+	"MWFjZTgwMTktMTk1Ni00ZTEyLWI1NjItMWUyNTQwYTBjYTI2",
 );
 
 module.exports = client;
