19587c50a222a4f9d45cd9c823bf89d9731d1e4f
Samandar <iwtuz166@gmail.com>
2024-05-13 13:47:18 +0500

    add check login password

--- middleware/payme.middleware.js
+++ middleware/payme.middleware.js
@@ -1,3 +1,8 @@
+const fs = require("fs");
+const {JSONRPCServer} = require("json-rpc-2.0");
+const RpcError = require("json-rpc-error");
+const server = new JSONRPCServer();
+
 async function PaymeMiddleware(req, res, next) {
 	const authorizationHeader = req.headers.authorization;
 	if (!authorizationHeader) {
@@ -15,8 +20,20 @@ async function PaymeMiddleware(req, res, next) {
 	}
 
 	try {
-		console.log(Buffer.from(accessToken, "base64").toString("ascii"));
-		return next();
+		fs.readFile("./db/payme.json", "utf8", (err, data) => {
+			if (err) {
+				console.error(err);
+				return res.status(500).json({error: "Failed to read file"});
+			}
+			const file = JSON.parse(data);
+			const decode = Buffer.from(accessToken, "base64")
+				.toString("ascii")
+				.split(":");
+			if (file.password != decode[1] || file.login != decode[0]) {
+				return res.status(500).json({error: "Failed to"});
+			}
+			return next();
+		});
 	} catch (error) {
 		return res
 			.status(500)
