5188bd53a38b5fb14a5b4af7b94ff6d146cbf903
Samandar <iwtuz166@gmail.com>
2024-06-17 16:44:57 +0500

    fix bug on cdn upload photo

--- cdn/index.js
+++ cdn/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const router = require("./router.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.static("cdn/public"));
 app.use(cors());
+
+mongoose
+	.connect("mongodb://localhost:27017/goshtbor")
+	.then(() => console.log("Connected to MongoDB"))
+	.catch((err) => console.error("Could not connect to MongoDB:", err));
 
 app.use("/api", router);
 
