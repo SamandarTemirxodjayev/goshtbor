5c96a68b9fa998b4a2a3d9c4fd5f81eca6deb00f
Samandar <iwtuz166@gmail.com>
2024-06-21 18:01:07 +0500

    fix bug on category update

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,12 +1,46 @@
 const Category = require("../models/Category");
-
+require("dotenv").config();
 exports.getAllCategories = async (req, res) => {
 	try {
-		const category = await Category.find();
+		const page = parseInt(req.query.page) || 1;
+		const perPage = parseInt(req.query.perPage) || 10;
+
+		const totalCount = await Category.countDocuments();
+		const totalPages = Math.ceil(totalCount / perPage);
+
+		const categories = await Category.find()
+			.skip((page - 1) * perPage)
+			.limit(perPage);
+
+		const url = process.env.URL || "http://localhost:3000";
+		const _meta = {
+			currentPage: page,
+			perPage: perPage,
+			totalCount: totalCount,
+			totalPages: totalPages,
+		};
+
+		const _links = {
+			self: `${url}/api/category?page=${page}&perPage=${perPage}`,
+			first: `${url}/api/category?page=1&perPage=${perPage}`,
+			prev:
+				page > 1
+					? `${url}/api/category?page=${page - 1}&perPage=${perPage}`
+					: null,
+			next:
+				page < totalPages
+					? `${url}/api/category?page=${page + 1}&perPage=${perPage}`
+					: null,
+			last: `${url}/api/category?page=${totalPages}&perPage=${perPage}`,
+		};
+
+		// Send the response with pagination info
 		return res.status(200).json({
 			status: 200,
 			message: "Kategoriyalar Muvaffaqiyatli Yuklab Olindi",
-			data: category,
+			data: categories,
+			_meta: _meta,
+			_links: _links,
 		});
 	} catch (error) {
 		return res.status(500).json({message: error.message});
@@ -45,15 +79,9 @@ exports.deleteCategory = async (req, res) => {
 };
 exports.updateCategory = async (req, res) => {
 	try {
-		const category = await Category.findByIdAndUpdate(
-			req.params.id,
-			{
-				name: req.body.name,
-			},
-			{
-				new: true,
-			},
-		);
+		const category = await Category.findByIdAndUpdate(req.params.id, req.body, {
+			new: true,
+		});
 		return res.status(200).json({
 			status: 200,
 			message: "Kategoriya Muvaffaqiyatli Yangilandi",
