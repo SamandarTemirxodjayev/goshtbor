61843833fb22106b751fd8fbcef1f7e773fc2cc4
Samandar <iwtuz166@gmail.com>
2024-07-31 15:35:36 +0500

    change chat url

--- frontend/callcentre/composables/baseUrl.js
+++ frontend/callcentre/composables/baseUrl.js
@@ -1,6 +1,6 @@
 export const BASE_URL = "https://v1.api.mobile.goshtbor.uz/api";
 export const CDN_URL = "https://cdn.goshtbor.uz/api";
-export const socketUrl = "ws://217.171.146.128:3031";
+export const socketUrl = "wss://wss.goshtbor.uz";
 
 // export const BASE_URL = "http://localhost:3000/api";
 // export const CDN_URL = "https://cdn.goshtbor.uz/api";
