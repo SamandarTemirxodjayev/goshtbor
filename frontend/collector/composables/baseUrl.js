7572f5f00bd785536c2b4376be078d872f3c0b44
Samandar <iwtuz166@gmail.com>
2024-07-31 15:49:07 +0500

    change order url

--- frontend/collector/composables/baseUrl.js
+++ frontend/collector/composables/baseUrl.js
@@ -1,6 +1,6 @@
 export const BASE_URL = "https://v1.api.mobile.goshtbor.uz/api";
 export const CDN_URL = "https://cdn.goshtbor.uz/api";
-export const socketUrl = "ws://217.171.146.128:3030";
+export const socketUrl = "wss://orders.goshtbor.uz";
 
 // export const BASE_URL = "http://localhost:3000/api";
 // export const CDN_URL = "https://cdn.goshtbor.uz/api";
