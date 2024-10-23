7f93ad6b6b13738efc7a916243f0ce4bdf427d2a
Samandar <iwtuz166@gmail.com>
2024-10-23 11:40:09 +0500

    update notification

--- routes/notification.routes.js
+++ routes/notification.routes.js
@@ -5,7 +5,7 @@ const controller = require("../controllers/notification.controller.js");
 const router = Router();
 
 router.post("/", adminMiddleware, controller.createNotification);
-router.get("/", userMiddleware, controller.getNotifications);
+router.get("/", controller.getNotifications);
 router.post("/:id/read", userMiddleware, controller.markNotificationAsRead);
 router.post("/readall", userMiddleware, controller.markAsReadAllNotifications);
 router.delete("/:id", adminMiddleware, controller.deleteNotification);
